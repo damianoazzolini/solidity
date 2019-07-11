a6b70780bd65df2d211414c03ae273e6c1f0eb34
chriseth <chris@ethereum.org>
2019-07-11 18:46:17 +0200

    Precompiles.

--- test/EVMHost.h
+++ test/EVMHost.h
@@ -159,7 +159,18 @@ public:
 	evmc_address m_coinbase = convertToEVMC(Address("0x7878787878787878787878787878787878787878"));
 
 private:
+	evmc::result precompileECRecover(evmc_message const& _message) noexcept;
 	evmc::result precompileSha256(evmc_message const& _message) noexcept;
+	evmc::result precompileRipeMD160(evmc_message const& _message) noexcept;
+	evmc::result precompileIdentity(evmc_message const& _message) noexcept;
+	evmc::result precompileModExp(evmc_message const& _message) noexcept;
+	evmc::result precompileALTBN128G1Add(evmc_message const& _message) noexcept;
+	evmc::result precompileALTBN128G1Mul(evmc_message const& _message) noexcept;
+	evmc::result precompileALTBN128PairingProduct(evmc_message const& _message) noexcept;
+	evmc::result precompileGeneric(evmc_message const& _message, std::map<bytes, bytes> const& _inOut) noexcept;
+	/// @returns a result object with no gas usage and result data taken from @a _data.
+	/// @note The data object needs to be statically allocated!
+	static evmc::result resultWithGas(evmc_message const& _message, bytes const& _data) noexcept;
 
 	evmc::vm& m_vm;
 	evmc_revision m_evmVersion;
